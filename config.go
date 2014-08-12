503b7177a8cf5dd9e927f75fad919a06f0d14a13
Michael Samuel <mik@miknet.net>
2014-08-13 09:23:26 +1000

    Tune for 64-bit hash breaking on 6-core machine

--- config.go
+++ config.go
@@ -1,13 +1,14 @@
 package main
 
 const (
-	HASH_TRUNC         = 7         // 64-bit
-	SPACE_WF           = (1 << 28) // ~ 80G of RAM
-	TIME_WF            = (1 << 29) // SPACE_WF * TIME_WF should be >= (1<<64)
-	STORE_PROCS        = 8
-	GENERATE_PROCS     = 8
-	VERIFY_PROCS       = 8
-	NUM_BUCKETS        = (SPACE_WF / STORE_PROCS * 4 / 3)
+	HASH_TRUNC         = 8         // 64-bit
+	SPACE_WF           = (1 << 32) // ~ 80G of RAM
+	TIME_WF            = (1 << 34) // SPACE_WF * TIME_WF should be >= (1<<64)
+	STORE_PROCS        = 128
+	GENERATE_PROCS     = 6
+	VERIFY_PROCS       = 6
+	NUM_BUCKETS        = (SPACE_WF / STORE_PROCS * 5 / 4)
 	CUCKOO_TTL         = 512
-	SIMUL_TABLE_BUILDS = 2
+	SIMUL_TABLE_BUILDS = 6
+	SEED_BYTES         = 4
 )
