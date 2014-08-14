7da4a7ad2d9e84968590ec42b98eefcdc35c4a50
Michael Samuel <mik@miknet.net>
2014-08-14 10:52:38 +1000

    Leave config in state that works on normal PCs

--- config.go
+++ config.go
@@ -1,14 +1,12 @@
 package main
 
 const (
-	HASH_TRUNC         = 8         // 64-bit
-	SPACE_WF           = (1 << 32) // ~ 80G of RAM
-	TIME_WF            = (1 << 34) // SPACE_WF * TIME_WF should be >= (1<<64)
-	STORE_PROCS        = 128
-	GENERATE_PROCS     = 6
-	VERIFY_PROCS       = 6
-	NUM_BUCKETS        = (SPACE_WF / STORE_PROCS * 5 / 4)
-	CUCKOO_TTL         = 512
-	SIMUL_TABLE_BUILDS = 6
-	SEED_BYTES         = 4
+	HASH_TRUNC     = 6         // 64-bit
+	SPACE_WF       = (1 << 24) // ~ 80G of RAM
+	TIME_WF        = (1 << 34) // SPACE_WF * TIME_WF should be >= (1<<64)
+	STORE_PROCS    = 8
+	GENERATE_PROCS = 8
+	VERIFY_PROCS   = 8
+	NUM_BUCKETS    = (SPACE_WF / STORE_PROCS)
+	SEED_BYTES     = 3
 )
