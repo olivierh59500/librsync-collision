a1ea2dec7a9da35ed7f509d80d489554eeaf91dd
Michael Samuel <mik@miknet.net>
2014-08-14 10:50:36 +1000

    Support scaling the start-code bitarray

---
+++ compression.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"sort"
+)
+
+type CompressedSequence struct {
+	FreqCounts []FrequencyCount
+	TScale     uint32
+}
+
+type FrequencyCount struct {
+	Count    uint32
+	Val      byte
+	CodeBits uint32
+	CodeVal  uint32
+}
+
+func NewCompressedSequence(freqCounts []uint32) (cs CompressedSequence) {
+	cs.FreqCounts = make([]FrequencyCount, len(freqCounts))
+
+	for i, count := range freqCounts {
+		cs.FreqCounts[i] = FrequencyCount{Count: count, Val: byte(i)}
+	}
+	sort.Sort(sort.Reverse(ByFrequency(cs.FreqCounts)))
+	return
+}
+
+func (self *CompressedSequence) AssignCodes(tScale uint32) {
+	var i uint32 = 0
+	self.TScale = tScale
+	for numBits := tScale; ; numBits += tScale {
+		for val := uint32(0); val < (1 << numBits); val++ {
+			if i >= uint32(len(self.FreqCounts)) {
+				return
+			}
+			self.FreqCounts[i].CodeBits = numBits
+			self.FreqCounts[i].CodeVal = val
+			i++
+		}
+	}
+}
+
+func (self CompressedSequence) EstimateSize() uint64 {
+	var ret uint64
+	for _, count := range self.FreqCounts {
+		codeSize := uint64(count.CodeBits) * uint64(count.Count)
+		ret += codeSize + (codeSize / uint64(self.TScale))
+	}
+	return (ret + 7) / 8
+}
+
+type ByFrequency []FrequencyCount
+
+func (a ByFrequency) Len() int           { return len(a) }
+func (a ByFrequency) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
+func (a ByFrequency) Less(i, j int) bool { return a[i].Count < a[j].Count }
--- hashtable.go
+++ hashtable.go
@@ -3,15 +3,12 @@ package main
 import (
 	"fmt"
 	"github.com/dchest/siphash"
-	"io"
-	"io/ioutil"
 	"math/rand"
-	"sort"
 )
 
 type HashTable struct {
-	hashes HashFunc
-	table  []DigestSeedHash
+	hashfunc HashFunc
+	table    []byte
 }
 
 type HashFunc struct {
@@ -20,199 +17,76 @@ type HashFunc struct {
 	M  uint64
 }
 
-type DigestSeedHash struct {
-	Digest []byte
-	Seed   []byte
-	H      int
-}
-
-type CompressedTable struct {
-	hashes HashFunc
-	table  []byte
-}
-
-type FrequencyCount struct {
-	Count    uint32
-	Val      byte
-	CodeBits byte
-	CodeVal  byte
-}
-
-func (self *HashFunc) Sum(data []byte) []uint64 {
-	h := siphash.Hash(self.K0, self.K1, data)
-	ret := make([]uint64, 3)
-	ret[0] = (h >> 32) % self.M
-	ret[1] = h % self.M
-	ret[2] = ((h >> 32) + h) % self.M
-	return ret
+func (self *HashFunc) Sum(data []byte) uint64 {
+	return siphash.Hash(self.K0, self.K1, data) % self.M
 }
 
 func NewHashTable(num_buckets uint64) (table HashTable) {
 	var r1 uint64 = (uint64(rand.Uint32()) << 32) | uint64(rand.Uint32())
 	var r2 uint64 = (uint64(rand.Uint32()) << 32) | uint64(rand.Uint32())
-	table.hashes = HashFunc{r1, r2, num_buckets}
-	table.table = make([]DigestSeedHash, num_buckets)
+	table.hashfunc = HashFunc{r1, r2, num_buckets}
+	table.table = make([]byte, num_buckets*SEED_BYTES)
 	return table
 }
 
-func sortFrequencyCounts(counts []uint32) []FrequencyCount {
-	sorted := make([]FrequencyCount, len(counts))
-	for i, count := range counts {
-		sorted[i] = FrequencyCount{Count: count, Val: byte(i)}
-	}
-	sort.Sort(sort.Reverse(ByFrequency(sorted)))
+func (self *HashTable) Insert(val *DigestSeed) {
+	offset := self.hashfunc.Sum(val.Digest) * SEED_BYTES
+	copy(self.table[offset:], encode_seed(val.Seed))
+}
 
-	var i uint32 = 0
-	for numBits := uint(1); ; numBits++ {
-		for val := uint(0); val < (1 << numBits); val++ {
-			if i >= uint32(len(sorted)) {
-				return sorted
-			}
-			sorted[i].CodeBits = byte(numBits)
-			sorted[i].CodeVal = byte(val)
-			i++
+func (self *HashTable) Get(key []byte) uint64 {
+	h := self.hashfunc.Sum(key)
+	start := h * SEED_BYTES
+	end := start + SEED_BYTES
+	return decode_seed(self.table[start:end])
+}
+
+func (self *HashTable) GetCompressedSize() (uint64, uint32) {
+	freqCounts := make([]uint32, 256)
+	for _, x := range self.table {
+		freqCounts[x]++
+	}
+	cs := NewCompressedSequence(freqCounts)
+
+	minSize := uint64(0xffffffffffffffff)
+	bestT := uint32(0)
+	for i := uint32(1); i < 8; i *= 2 {
+		cs.AssignCodes(i)
+		sizeEstimate := cs.EstimateSize()
+		if sizeEstimate < minSize {
+			minSize, bestT = sizeEstimate, i
 		}
+		send_status(fmt.Sprintf("Size with tscale %v: %v", sizeEstimate, i))
 	}
+	cs.AssignCodes(bestT)
+
+	return minSize, bestT
 }
 
-func num_compressed_bits(counts []FrequencyCount) uint64 {
-	var ret uint64
-	for _, count := range counts {
-		ret += (uint64(count.CodeBits) * uint64(count.Count) * 2)
-	}
-	return ret
+func (self *HashTable) GetUncompressedSize() uint64 {
+	return uint64(len(self.table))
 }
 
-type ByFrequency []FrequencyCount
-
-func (a ByFrequency) Len() int           { return len(a) }
-func (a ByFrequency) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
-func (a ByFrequency) Less(i, j int) bool { return a[i].Count < a[j].Count }
-
-func (self *HashTable) displace(val *DigestSeedHash, ttl int) bool {
-	if ttl <= 0 {
-		return false
-	}
-
-	ok := true
-	hs := self.hashes.Sum(val.Digest)
-	h := hs[val.H%len(hs)]
-
-	displaced := self.table[h]
-	self.table[h] = *val
-	if displaced.Digest != nil {
-		displaced.H++
-		ok = self.displace(&displaced, ttl-1)
-	}
-
-	return ok
-}
-
-func (self *HashTable) Insert(val *DigestSeedHash) bool {
-	return self.displace(val, CUCKOO_TTL)
-}
-
-func (self *HashTable) Compress() CompressedTable {
-	ret := CompressedTable{self.hashes, make([]byte, len(self.table)*SEED_BYTES)}
-	for i := 0; i < len(self.table); i++ {
-		if self.table[i].Seed != nil {
-			copy(ret.table[i*SEED_BYTES:], self.table[i].Seed)
-		}
-	}
-	return ret
-}
-
-func (self *CompressedTable) Get(key []byte) []uint64 {
-	ret := make([]uint64, 3)
-	for i, h := range self.hashes.Sum(key) {
-		start := h * SEED_BYTES
-		end := start + SEED_BYTES
-		ret[i] = decode_seed(self.table[start:end])
-	}
-	return ret
-}
-
-func read_from_tempfile(tempfile io.Reader, ch chan<- DigestSeedHash) {
-	defer close(ch)
-
-	for {
-		buf := make([]byte, HASH_TRUNC+SEED_BYTES)
-		var n int = 0
-		var err error
-		for n < HASH_TRUNC+SEED_BYTES && err == nil {
-			var m int
-			m, err = tempfile.Read(buf[n:])
-			n += m
-		}
-		if n == HASH_TRUNC+SEED_BYTES {
-			ch <- DigestSeedHash{Digest: buf[:HASH_TRUNC], Seed: buf[HASH_TRUNC:]}
-		}
-		if err != nil {
-			return
-		}
-	}
-}
-
-func make_hashtable(inchan <-chan *DigestSeed) CompressedTable {
-	tempfile, err := ioutil.TempFile(".", "hashstorage")
-	if err != nil {
-		panic(err)
-	}
-
+func make_hashtable(inchan <-chan *DigestSeed) HashTable {
+	htable := NewHashTable(NUM_BUCKETS)
 	var count uint64
 
-	freqCounts := make([]uint32, 256)
-
 	for data := range inchan {
-		seed := encode_seed(data.Seed)
-		for _, x := range seed {
-			freqCounts[x]++
-		}
-
-		buf := make([]byte, HASH_TRUNC+SEED_BYTES)
-		copy(buf, data.Digest)
-		copy(buf[HASH_TRUNC:], seed)
-		if _, err := tempfile.Write(buf); err != nil {
-			panic("Unable to write to tempfile")
-		}
-
+		htable.Insert(data)
 		if count++; count&0xfffff == 0xfffff {
 			send_status(fmt.Sprintf("Collected %d hashes", count))
 		}
 	}
 
-	sortedCounts := sortFrequencyCounts(freqCounts)
-	send_status(fmt.Sprintf("Approx compressed/uncompressed size: %v / %v", num_compressed_bits(sortedCounts)/8, NUM_BUCKETS*SEED_BYTES))
+	compressedSize, bestT := htable.GetCompressedSize()
+	send_status(fmt.Sprintf("Approx compressed/uncompressed size: %v / %v [tscale=%v]", compressedSize, htable.GetUncompressedSize(), bestT))
 
-	<-TableBuildSem
-
-	if _, err := tempfile.Seek(0, 0); err != nil {
-		panic("Unable to seek to the start of the tempfile")
-	}
-
-	htable := NewHashTable(NUM_BUCKETS)
-	count = 0
-	var collision_count uint64
-	ch := make(chan DigestSeedHash, (1 << 8))
-	go read_from_tempfile(tempfile, ch)
-
-	for data := range ch {
-		if ok := htable.Insert(&data); !ok {
-			collision_count += 1
-		}
-		if count++; count&0xfffff == 0xfffff {
-			send_status(fmt.Sprintf("Inserted %d hashes", count))
-		}
-	}
-	send_status(fmt.Sprintf("Finished collecting hashes, %d received, %d collisions", count, collision_count))
-	return htable.Compress()
+	return htable
 }
 
-func bucket_finder(htable CompressedTable, inchan <-chan *DigestSeed, verify_chan chan<- Candidate) {
-	TableBuildSem <- struct{}{}
+func bucket_finder(htable HashTable, inchan <-chan *DigestSeed, verify_chan chan<- Candidate) {
 	for data := range inchan {
-		for _, seed := range htable.Get(data.Digest) {
-			verify_chan <- Candidate{seed, data}
-		}
+		seed := htable.Get(data.Digest)
+		verify_chan <- Candidate{seed, data}
 	}
 }
